2933ef0db125848825c59ad7b7ded1505264d941
trainer5-ctrl <trainer5@gamelab.id>
2026-03-18 13:00:48 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,31 +1,12 @@
 const canvas = document.getElementById('canvas');
-const person = new Person(canvas);
-
-function gameLoop() {
-    // 1. Bersihkan Kanvas
-    const ctx = canvas.getContext('2d');
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // 2. Update Animasi Karakter
-    person.update();
-
-    // 3. Gambar Karakter
-    person.draw();
-
-    // 4. Gambar objek lain (Tree, Lumberjack, dll)
-    // tree.draw(); 
-
-    requestAnimationFrame(gameLoop);
-}
-
-// Jalankan loop
-gameLoop();
-
-// Link-kan dengan tombol HTML Anda
-document.getElementById('move-left').addEventListener('click', () => {
-    person.moveLeft();
+let btnLeft = document.getElementById('move-left');
+let btnRight = document.getElementById('move-right');
+console.log(window.innerHeight)
+const game = new Lumberjack({
+    el: canvas,
+    maxWidth: 600,
+    maxHeight: window.innerHeight - 150,
+    btnLeft, btnRight
 });
-
-document.getElementById('move-right').addEventListener('click', () => {
-    person.moveRight();
-});
+game.init()
+game.render();
