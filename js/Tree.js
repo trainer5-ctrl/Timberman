85a3144e24df85dbfe640860e7718f146152577d
trainer5-ctrl <trainer5@gamelab.id>
2026-03-25 13:42:53 +0700

    Update Tree.js

--- js/Tree.js
+++ js/Tree.js
@@ -7,9 +7,9 @@ class Tree {
         this.width = 100;
         this.height = 150;
         this.trees = [];
-        this.treesPossibility = [0, "left", "right"];
+        this.treesPossibility = [0,"left","right"];
         this.trunkColors = ['#a17438','#cc8e35'];
-        this.stoneColor = '#7f8c8d';
+        this.stoneColor = 'grey';
         this.stemWidth = 100;
         this.stemHeight = 30;
         this.starterTree = 5;
@@ -24,42 +24,13 @@ class Tree {
     }
 
     createNewTrunk() {
-        let lastColor = this.trees[this.trees.length - 1].color;
+        let lastColor = this.trees[this.trees.length-1].color;
         let color = lastColor === this.trunkColors[0] ? this.trunkColors[1] : this.trunkColors[0];
         let newTrunk = this.treesPossibility[randomNumber(3)];
         this.trees.push({ value: newTrunk, color });
     }
 
-    drawTrunk(x, y, width, height, color) {
-        let grad = this.ctx.createLinearGradient(x, y, x, y + height);
-        grad.addColorStop(0, color);
-        grad.addColorStop(1, "#5d3a1a");
-        this.ctx.fillStyle = grad;
-        this.ctx.fillRect(x, y, width, height);
-
-        this.ctx.strokeStyle = "#4b2e1a";
-        this.ctx.lineWidth = 2;
-        for(let i = 5; i < width; i += 15){
-            this.ctx.beginPath();
-            this.ctx.moveTo(x + i, y);
-            this.ctx.lineTo(x + i + randomNumber(5)-2, y + height);
-            this.ctx.stroke();
-        }
-    }
-
-    drawLeaves(x, y, width) {
-        let leafCount = randomNumber(3) + 2;
-        for(let i = 0; i < leafCount; i++){
-            let leafX = x + width/2 + (randomNumber(60)-30);
-            let leafY = y - (randomNumber(50)+20);
-            this.ctx.fillStyle = `rgba(46, 204, 113, ${0.6 + Math.random()*0.4})`;
-            this.ctx.beginPath();
-            this.ctx.ellipse(leafX, leafY, 20+randomNumber(10), 15+randomNumber(10), Math.PI/4, 0, 2*Math.PI);
-            this.ctx.fill();
-        }
-    }
-
-    // Fungsi helper: rectangle dengan sudut membulat
+    // Fungsi helper untuk rectangle dengan sudut membulat
     roundRect(x, y, w, h, r) {
         this.ctx.beginPath();
         this.ctx.moveTo(x + r, y);
@@ -76,37 +47,26 @@ class Tree {
     }
 
     draw() {
-        let x = this.canvas.width / 2 - this.width / 2;
+        let x = this.canvas.width/2 - this.width/2;
 
-        this.trees.forEach((tree, index) => {
-            let trunkY = this.startY - (index * this.height);
-            this.drawTrunk(x, trunkY, this.width, this.height, tree.color);
+        this.trees.forEach((tree,index) => {
+            this.ctx.fillStyle = tree.color;
+            this.ctx.fillRect(x, this.startY - (index * this.height), this.width,this.height);
 
-            // Ranting kiri
-            if(tree.value === 'left') {
+            if(tree.value == 'left') {
                 this.ctx.fillStyle = "#8e5a2c";
-                this.roundRect(x - this.stemWidth, trunkY + this.height/2, this.stemWidth, this.stemHeight, 10);
-                this.drawLeaves(x - this.stemWidth, trunkY + this.height/2, this.stemWidth);
+                this.roundRect(x-this.stemWidth, this.startY - (index * this.height) + this.height/2, this.stemWidth,this.stemHeight, 10);
             }
-
-            // Ranting kanan
-            if(tree.value === 'right') {
+            if(tree.value == 'right') {
                 this.ctx.fillStyle = "#8e5a2c";
-                this.roundRect(x + this.width, trunkY + this.height/2, this.stemWidth, this.stemHeight, 10);
-                this.drawLeaves(x + this.width, trunkY + this.height/2, this.stemWidth);
-            }
-
-            // Daun puncak
-            if(index === this.trees.length - 1){
-                this.drawLeaves(x, trunkY, this.width);
+                this.roundRect(x+this.width, this.startY - (index * this.height) + this.height/2, this.stemWidth,this.stemHeight, 10);
             }
         });
 
-        // Batu bawah pohon
         this.ctx.fillStyle = this.stoneColor;
-        this.roundRect(x - 10, this.startY + this.height - 10, 50, 30, 10);
+        this.roundRect(x-10, this.startY+this.height-10, 50, 30, 10);
         this.ctx.fillStyle = '#95a5a6';
-        this.roundRect(x + 20, this.startY + this.height - 10, 80, 30, 10);
+        this.roundRect(x+20, this.startY+this.height-10, 80, 30, 10);
     }
 }
 
